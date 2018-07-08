a23d120527bb226860f983dfbca54f2e94e7dc59
MichaelBone <Michael.Bone@senet.com.au>
2018-07-08 21:02:21 +0930

    Updated the SSL version.

--- scraper.js
+++ scraper.js
@@ -52,7 +52,7 @@ function insertRow(database, developmentApplication) {
     
 function requestPage(url, callback) {
     console.log(`Requesting page: ${url}`);
-    request.get({ uri: url, agentOptions: { secureProtocol: "TLSv1_3_method" } }, (error, response, body) => {
+    request.get({ uri: url, agentOptions: { secureProtocol: "TLSv1" } }, (error, response, body) => {
         if (error)
             console.log(`Error requesting page ${url}: ${error}`);
         else
