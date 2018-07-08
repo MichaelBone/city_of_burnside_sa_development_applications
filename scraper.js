e9b9c32546173395e07efd614ee909f23fd70695
MichaelBone <Michael.Bone@senet.com.au>
2018-07-08 21:44:53 +0930

    Updated SSL version.

--- scraper.js
+++ scraper.js
@@ -54,7 +54,7 @@ function insertRow(database, developmentApplication) {
     
 function requestPage(url, callback) {
     console.log(`Requesting page: ${url}`);
-    request.get({ uri: url, agentOptions: { secureProtocol: "TLSv1_method", port: "443", strictSSL: false, rejectUnauthorized: false } }, (error, response, body) => {
+    request.get({ uri: url, agentOptions: { secureProtocol: "SSLv3_method", port: "443", strictSSL: false, rejectUnauthorized: false } }, (error, response, body) => {
         if (error)
             console.log(`Error requesting page ${url}: ${error}`);
         else
