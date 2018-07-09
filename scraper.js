bc9203cd62a19349b9109affb707ce203490f74c
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-07-09 11:44:25 +0930

    Changed back to headless.

--- scraper.js
+++ scraper.js
@@ -81,7 +81,7 @@ function run(database) {
         console.log(result);
     });
 
-    puppeteer.launch({ headless: false, ignoreHTTPSErrors: true }).then(function(browser) {
+    puppeteer.launch({ headless: true, args: [ '--no-sandbox', '--disable-setuid-sandbox' ] ignoreHTTPSErrors: true }).then(function(browser) {
         console.log("Here A1");
         browser.newPage().then(function(page) {
             console.log("Here A2");
