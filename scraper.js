c3b0a8164fa87709fdcc8f924860f2780d62914a
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-07-09 19:11:09 +0930

    Reverted to version that works on client PC.

--- scraper.js
+++ scraper.js
@@ -4,21 +4,11 @@
 // Michael Bone
 // 8th July 2018
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
 let cheerio = require("cheerio");
 let request = require("request");
 let sqlite3 = require("sqlite3").verbose();
 let urlparser = require("url");
 let moment = require("moment");
-// let phantom = require("phantom");
-let selenium = require("selenium-webdriver");
-let chrome = require("selenium-webdriver/chrome");
-let puppeteer = require("puppeteer");
-let https = require("https");
-let sslRootCas = require('ssl-root-cas/latest').inject();
-
-https.globalAgent.options.ca = sslRootCas;
 
 const DevelopmentApplicationsUrl = "https://www.burnside.sa.gov.au/Planning-Business/Planning-Development/Development-Applications/Development-Applications-on-Public-Notification";
 const CommentUrl = "mailto:burnside@burnside.sa.gov.au";
@@ -62,7 +52,7 @@ function insertRow(database, developmentApplication) {
     
 function requestPage(url, callback) {
     console.log(`Requesting page: ${url}`);
-    request.get({url: url, agentOptions: { secureProtocol: "TLSv1_2_method" } }, (error, response, body) => {
+    request(url, (error, response, body) => {
         if (error)
             console.log(`Error requesting page ${url}: ${error}`);
         else
@@ -76,74 +66,7 @@ function run(database) {
     let url = DevelopmentApplicationsUrl;
     let parsedUrl = new urlparser.URL(url);
     let baseUrl = parsedUrl.origin + parsedUrl.pathname;
-    
-    console.log("Testing using selenium.");
-    
-    let options = new chrome.Options();
-    options.addArguments("headless");
-    let driver = new selenium.Builder().forBrowser('chrome').setChromeOptions(options).build();
-    driver.get(url).then(function (result) {
-        console.log("Have page.");
-        console.log(result);
-    });
 
-    puppeteer.launch({ headless: true, args: [ '--no-sandbox', '--disable-setuid-sandbox', '--ignore-certificate-errors', '--ignore-urlfetcher-cert-requests' ], ignoreHTTPSErrors: true }).then(function(browser) {
-        // console.log("Here A0");
-        // require('https').globalAgent.options.ca = sslRootCas;
-        console.log("Here A1");
-        browser.newPage().then(function(page) {
-            console.log("Here A2");
-            page.goto('https://www.chromestatus.com', { waitUntil: 'networkidle2' }).then(function(page) {
-                console.Log("Here A3");
-                page.pdf({path: 'page.pdf', format: 'A4'});
-            });
-        });
-    });
-
-    
-// phantom.create().then(function(ph){
-//     _ph = ph;
-//     console.log("Here 1");    
-//     return _ph.createPage();
-// }).then(function(page){
-//     _page = page;
-//     console.log("Here 2: " + url);
-//     page.customHeaders = {
-//         "Connection": "keep-alive"
-//     };
-//     page.on('onResourceError', function(resourceError) {
-//         page.reason = resourceError.errorString;
-//         page.reason_url = resourceError.url;
-//         console.log("ErrorTest1: " + resourceError.errorString);
-//         console.log("ErrorTest2: " + resourceError.url);
-//     });
-//     return _page.open(url,
-//         function (status) {
-//         if (status !== 'success') {
-//             console.log("Error opening url \"" + page.reason_url + "\": " + page.reason);
-//             phantom.exit(1);
-//         } else {
-//             console.log("Successful page open.");
-//             phantom.exit(0);
-//         }
-//     });
-// }).then(function(status){
-//    console.log("Here 3");
-//      console.log(status);
-//     return _page.property('content')
-// }).then(function(content){
-//     console.log("Here 4");
-//     console.log(content);
-//     _page.close();
-//     _ph.exit();
-// }).catch(function(e){
-//     console.log("Here 5");
-//     console.log(e); 
-// });
-// console.log("Done.");
-return;
-    
-    
     requestPage(url, body => {
         // Use cheerio to find all development applications listed in the page.
  
@@ -174,30 +97,4 @@ return;
     });
 }
 
-function function2() {
-    console.log("Running function2");
-    https.globalAgent.options.ca = sslRootCas;
-    const options = {
-        hostname: "www.burnside.sa.gov.au",
-        port: 443,
-        path: "/Planning-Business/Planning-Development/Development-Applications/Development-Applications-on-Public-Notification",
-        method: "GET",
-        secureProtocol: "TLSv1_method",
-        rejectUnauthorized: false,
-        requestCert: true,
-        agent: false,
-        headers: { 'User-Agent': 'Mozilla/5.0' }
-    };
-    https.request(options, res => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-        res.on('data', (d) => {
-            process.stdout.write(d);
-        });
-        initializeDatabase(run);
-    }).on('error', e => { console.log(e); });
-}
-
-setTimeout(function2, 15000);
-
-
+initializeDatabase(run);
