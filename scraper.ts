aa8dd13bfe64ad51ad7c4fc5fb49b2ed806c96b6
MichaelBone <Michael.Bone@senet.com.au>
2018-08-13 21:43:24 +0930

    Added pauses.

--- scraper.ts
+++ scraper.ts
@@ -183,6 +183,7 @@ async function parsePdf(url: string) {
     // Read the PDF.
 
     let buffer = await request({ url: url, encoding: null, proxy: process.env.MORPH_PROXY });
+    await sleep(2000 + getRandom(0, 5) * 1000);
 
     // Parse the PDF.  Each page has details of a single application (which in some cases may
     // overflow onto subsequent pages).
@@ -244,6 +245,12 @@ function getRandom(minimum: number, maximum: number) {
     return Math.floor(Math.random() * (Math.floor(maximum) - Math.ceil(minimum))) + Math.ceil(minimum);
 }
 
+// Pauses for the specified number of milliseconds.
+
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
 // Parses the page at the specified URL.
 
 async function main() {
@@ -256,6 +263,7 @@ async function main() {
     console.log(`Retrieving page: ${DevelopmentApplicationsUrl}`);
     let body = await request({ url: DevelopmentApplicationsUrl, proxy: process.env.MORPH_PROXY });
     let $ = cheerio.load(body);
+    await sleep(2000 + getRandom(0, 5) * 1000);
 
     let pdfUrls: string[] = [];
     for (let element of $("a.ext-pdf[href$='.pdf']").get()) {
